375f3889bc7d3c564648b61089599caae6ff72f8
ArcaneWD <harsha2004alpur@gmail.com>
2026-02-11 11:07:00 +0530

    added demo routes for testing

--- src/utils/asyncHandler.utils.js
+++ src/utils/asyncHandler.utils.js
@@ -1,5 +1,5 @@
-const acyncHandler = (requestHandler) => {
-  (req, res, next) => {
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
